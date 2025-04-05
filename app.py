90eb47165f2bc0aa909484cc64d3f2505c7ccaa2
H K <98111277+habo3hnk@users.noreply.github.com>
2025-04-05 02:10:19 +0000

    add HOST and PORT to config

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import uvicorn
 
 from fastapi import FastAPI
 from api.routers import images
+from config.config import HOST, PORT
 
 
 app = FastAPI()
@@ -17,4 +18,4 @@ logging.basicConfig(
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host=HOST, port=PORT)
